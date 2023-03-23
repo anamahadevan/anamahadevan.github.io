69169c5bceac86d12b2bcac50e353de1ca5bcb2a
ana <anabellemahadevan@gmail.com>
2023-03-23 14:12:55 -0600

    eep

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -5,14 +5,20 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const photo_arr = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
+const photo_text = ["pic 1","pic 1","pic 1","pic 1","pic 1"];
+
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for(let i =0;i>0;i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
+    i++;
+}
 
 /* Wiring up the Darken/Lighten button */

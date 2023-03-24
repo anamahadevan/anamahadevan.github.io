be0b00ec8521fb84af8cf7517036721e4275d894
ana <anabellemahadevan@gmail.com>
2023-03-23 20:45:34 -0600

    meep

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -9,15 +9,36 @@ const photo_arr = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
-const photo_text = ["pic 1","pic 1","pic 1","pic 1","pic 1"];
+const photo_txt = ["pic 1","pic 1","pic 1","pic 1","pic 1"];
 
 /* Looping through images */
 
-for(let i =0;i>0;i++){
+for(let i =0;i<=5;i++){
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', xxx);
-    newImage.setAttribute('alt', xxx);
+    newImage.setAttribute('src', photo_arr[i]);
+    newImage.setAttribute('alt', photo_txt[i]);
     thumbBar.appendChild(newImage);
+    newImage.addEventListener('click',clck);
 }
 
+clck(){
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+}
+
+
+
+
+    
+
+
+
+/* Add click event listeners to thumb bar */
+// thumbBar.addEventListener("click", myFunction); 
+
 /* Wiring up the Darken/Lighten button */
+
+darken(){
+    
+}
+

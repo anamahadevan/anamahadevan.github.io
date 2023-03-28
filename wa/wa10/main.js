a376d6b80e8191c560eaf3175c303424eb2c22c6
ana <anabellemahadevan@gmail.com>
2023-03-27 18:23:09 -0600

    kk

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const photo_arr = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
+const photo_arr = ['images/pic1.jpg','images/pic2.jpg', 'images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
@@ -13,7 +13,7 @@ const photo_txt = ["pic 1","pic 1","pic 1","pic 1","pic 1"];
 
 /* Looping through images */
 
-for(let i =0;i<=5;i++){
+for(let i =0;i<=4;i++){
     const newImage = document.createElement('img');
     newImage.setAttribute('src', photo_arr[i]);
     newImage.setAttribute('alt', photo_txt[i]);
@@ -21,16 +21,10 @@ for(let i =0;i<=5;i++){
     newImage.addEventListener('click',clck);
 }
 
-clck(){
+function clck(e){
     displayedImage.src = e.target.src;
     displayedImage.alt = e.target.alt;
 }
-
-
-
-
-    
-
 
 
 /* Add click event listeners to thumb bar */
@@ -38,7 +32,22 @@ clck(){
 
 /* Wiring up the Darken/Lighten button */
 
-darken(){
-    
-}
+btn.onclick = function dark_light() {
+    var btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark')  // make light
+    {
 
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } 
+    
+    else if(btnClass === 'light') //make darj
+    {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  }
+  
+  

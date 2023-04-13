214b9cb6b330ef8c80c92f0872b4bbc10a139e47
ana <anabellemahadevan@gmail.com>
2023-04-13 14:10:58 -0600

    push!

---
+++ js/wa12.js
@@ -0,0 +1,38 @@
+// creating json within 
+
+var employees = // variable with strings for json object
+
+{
+    employees: [
+    {
+    "firstname": "Sam",
+    "department": "Tech",
+    "designation": "Manager",
+    "salary": "40000",
+    "raise_eligible": "true"
+    },
+    {
+    "firstname": "Mary",
+    "department": "Finance",
+    "designation": "Trainee",
+    "salary": "18500",
+    "raise_eligible": "true"
+    },
+    {
+    "firstname": "Bill",
+    "department": "HR",
+    "designation": "Executive",
+    "salary": "21200",
+    "raise_eligible": "false"
+    },
+
+    ]
+}
+
+var company =
+{   
+    "company_name": "Tech Stars",
+    "website": "www.techstars.site",
+    "employees": [],
+}
+

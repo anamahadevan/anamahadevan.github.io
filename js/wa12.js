8424627b87a6bbff10435e4c6cba6cd41703c1d8
ana <anabellemahadevan@gmail.com>
2023-04-13 15:49:04 -0600

    rawr

--- js/wa12.js
+++ js/wa12.js
@@ -65,12 +65,46 @@ console.log('problem 5:')
 
 function raiseten() {
     for (i in company.employees) {
-        if (i.raise_eligible = true){
-        i.salary = i.salary * 1.1;
-        i.raise_eligible = false; // reset to false again so it doesnt keep doing the same?
+        if (company["employees"][i]["raise_eligible"] === "true"){ // if they are eligible for a raise THEN
+            company["employees"][i]["salary"] = company["employees"][i]["salary"]  * 1.1;
+            company["employees"][i]["raise_eligible"]; // reset to false again so it doesnt keep doing the same?
         }
     }
 }
 raiseten(); 
+
 console.log(company);
 
+//problem 6: for each employee, add  property 'wfh' and set it to true of false
+// console.log('problem 6:')
+
+// function workfromhome(wfh_status) {
+//     for (let i = 0; i < company.employees.length; i++) { // go through everyone
+//         // if theyre found in the parameters
+//         if(){
+//             let employee = company.employees[i]; // checks if correct 
+//             let is_wfh = wfh_status.employee.firstname;
+//             employee.working_from_home = is_wfh;
+//         }
+//         else if(){ // setting that others are not working from home 
+
+//         }
+
+//     }
+//     console.log(company);
+// }
+// workfromhome(["Anna","Sam"]);
+
+console.log('problem 6:')
+function workfromhome(wfh_status) {
+    for (let i = 0; i < company.employees.length; i++) {
+        if(company["employees"][i]["firstname"])// name is true
+        {
+            let employee = company["employees"][i];
+            let is_working = wfh_status.includes(company["employees"][i]["firstname"]) ? true : false;// shortened if statement here
+            employee.working_from_home = is_working;
+        }
+    }
+console.log(company);
+}
+workfromhome(["Anna","Sam"])

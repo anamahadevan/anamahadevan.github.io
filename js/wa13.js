0c0d1f0916ec695df6297778f29781ea4a91c246
ana <anabellemahadevan@gmail.com>
2023-04-20 15:36:38 -0600

    YESSSS BITCH I GOT IT

--- js/wa13.js
+++ js/wa13.js
@@ -1,256 +1,192 @@
-var userHP = 100;
-var opHP = 100;
-opAttacks = [flameThrower, dragonClaw, ember, growl];
-playerMove = 0;
+// var userHP = 100;
+// var opHP = 100;
+// opAttacks = [flameThrower, dragonClaw, ember, growl];
+// playerMove = 0;
+
 /* users moves */
 function volumeincr() {
-  if(playerMove == 0 && userHP != 0) {
-    var miss = Math.floor((Math.random() * 10) + 1); // miss rate
-    if(miss == 1) {
-      document.getElementById('message').innerHTML = " Blastoise's attack missed! ";
-    }
-    else {
-      document.getElementById('message').innerHTML = " Sylveon increased volume! "; // attack
-      var critical = Math.floor((Math.random() * 10) + 1); // critical
-      if(critical == 4){
-        for(var x = 0; x < 2; x++){
-          opHP = opHP - 30; // yes critical
-        }
-      }
-      else{
-        opHP = opHP - 30; // no critical
-      }
-      if(opHP < 0){ opHP = 0} //faint
-        document.getElementById('apHP').innerHTML = opHP; // update hp
-      if(opHP == 0){
-        document.getElementById('message').innerHTML = " Charizard fainted! " // update message
-      }
-    }
-    //wait();
-    playerMove = 1; // update player move
-}
+  document.getElementById('message').innerHTML = " sylveon increased volume! ";
+  updatehealth(10);
 }
 
 function waterPulse() {
-  if(playerMove == 0 && userHP != 0) {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Blastoise's attack missed! "
-  }
-  else {
-    document.getElementById('message').innerHTML = " Blastoise used water pulse ";
-    var critical = Math.floor((Math.random() * 10) + 1);
-      if(critical == 4){
-        for(var x = 0; x < 2; x++){
-          opHP = opHP - 20;
-        }
-      }
-      else{
-        opHP = opHP - 20;
-      }
-    if(opHP < 0 ) { opHP = 0}
-    document.getElementById('apHP').innerHTML = opHP;
-    //document.getElementById('message').innerHTML = " Charizard2 "
-    if(opHP == 0){
-      document.getElementById('message').innerHTML = " Charizard fainted! "
-    }
-  }
-  //wait();
-  playerMove = 1;
-}
+  document.getElementById('message').innerHTML = " sylveon missed! <br> volume decreased "
+  updatehealth(-10);
 }
 
 function surf() {
-  if(playerMove == 0 && userHP != 0) {
-  //alert("Water Cannon!");
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Blastoise's attack missed! "
-  }
-  else {
-    document.getElementById('message').innerHTML = " Blastoise used surf ";
-    var critical = Math.floor((Math.random() * 10) + 1);
-      if(critical == 4){
-        for(var x = 0; x < 2; x++){
-          opHP = opHP - 10;
-        }
-      }
-      else{
-        opHP = opHP - 10;
-      }
-    if(opHP < 0 ) { opHP = 0}
-    document.getElementById('apHP').innerHTML = opHP;
-    if(opHP == 0){
-      document.getElementById('message').innerHTML = " Charizard fainted! "
-    }
-  }
-  //wait();
-  playerMove = 1;
+  document.getElementById('message').innerHTML = " sylveon just pounded your mom! "
+  updatehealth(-5);
 }
-}
+
 function tackle() {
-  if(playerMove == 0 && userHP != 0) {
-  //alert("Water Cannon!");
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Blastoise's attack missed! "
-  }
-  else {
-    document.getElementById('message').innerHTML = " Blastoise used tackle ";
-    var critical = Math.floor((Math.random() * 10) + 1);
-      if(critical == 4){
-        for(var x = 0; x < 2; x++){
-          opHP = opHP - 5;
-        }
-      }
-      else{
-        opHP = opHP - 5;
-      }
-    if(opHP < 0){ opHP = 0}
-    document.getElementById('apHP').innerHTML = opHP;
-  //document.getElementById('message').innerHTML = " Charizard4 "
-    if(opHP == 0){
-      document.getElementById('message').innerHTML = " Charizard fainted! "
-    }
-  }
-  //wait();
-  playerMove = 1;
-}
+  document.getElementById('message').innerHTML = " erereree rawr "
+  updatehealth(30);
 }
 
 
 
-/* opponent's moves */
+// Get the health bar progress element
+let healthBarProgress = document.getElementById('health-bar-progress');
+let total_health = 0;
 
-function flameThrower() {
-  var miss = Math.floor((Math.random() * 10) + 1); // miss rate
-  if(miss == 1 ) {
-  document.getElementById('message').innerHTML = " Charizard's attack missed! " // attack missed
+function updatehealth(health){
+  total_health += health;
+
+  console.log("health:"+health);
+  console.log("total health:"+total_health);
+  // healthBarProgress.width = health;
+  if(health){
+    document.getElementById("health-bar-progress").style.width = health + '%';
   }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used flame thrower " // attack
-    var critical = Math.floor((Math.random() * 10) + 1); // critical
-      if(critical == 4){
-        for(var x = 0; x < 2; x++){ // yes critical
-          userHP = userHP - 30;
-        }
-      }
-      else{
-        userHP = userHP - 30; // no critical
-      }
-  if(userHP < 0) { userHP = 0} // faint
-  document.getElementById('myHP').innerHTML = userHP; // update hp
-    if(userHP == 0) { // fainted
-      document.getElementById('message').innerHTML = " volume at zero! " // fainted
-    }
-  }
+
 }
 
-function dragonClaw() {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Charizard's attack missed! "
-  }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used dragon claw "
-  var critical = Math.floor((Math.random() * 10) + 1);
-    if(critical == 4){
-      for(var x = 0; x < 2; x++){
-        userHP = userHP - 20;
-      }
-    }
-    else{
-      userHP = userHP - 20;
-    }
-  if(userHP < 0) { userHP = 0}
-  document.getElementById('myHP').innerHTML = userHP;
-    if(userHP == 0){
-      document.getElementById('message').innerHTML = " Blastoise fainted! "
-    }
-  }
-}
-
-function ember() {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Charizard's attack missed! "
-  }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used ember "
-  var critical = Math.floor((Math.random() * 10) + 1);
-    if(critical == 4){
-      for(var x = 0; x < 2; x++){
-        userHP = userHP - 10;
-      }
-    }
-    else{
-      userHP = userHP - 10;
-    }
-  if(userHP < 0) { userHP = 0}
-  document.getElementById('myHP').innerHTML = userHP;
-    if(userHP == 0){
-      document.getElementById('message').innerHTML = " Blastoise fainted! "
-    }
-  }
-}
-
-function growl() {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Charizard's attack missed! "
-  }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used growl "
-  var critical = Math.floor((Math.random() * 10) + 1);
-    if(critical == 4){
-      for(var x = 0; x < 2; x++){
-        userHP = userHP - 5;
-      }
-    }
-    else{
-      userHP = userHP - 5;
-    }
-  if(userHP < 0) { userHP = 0}
-  document.getElementById('myHP').innerHTML = userHP;
-    if(userHP == 0){
-      document.getElementById('message').innerHTML = " Blastoise fainted! "
-    }
-  }
-}
+updatehealth(0); // call function
 
 
 
-function compPokemon() { // continue
-  if(playerMove == 1 && opHP != 0) { // whos move
-  var move = Math.floor((Math.random() * 4) + 1); // choose move randomly
-    opAttacks[move](); // call attack from array
-    playerMove = 0; // update player move
-  }
-}
+
+
+
+
+// /* opponent's moves */
+
+// function flameThrower() {
+//   var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+//   if(miss == 1 ) {
+//   document.getElementById('message').innerHTML = " anthony's attack missed! " // attack missed
+//   }
+//   else {
+//   document.getElementById('message').innerHTML = " anthony used flame thrower " // attack
+//     var critical = Math.floor((Math.random() * 10) + 1); // critical
+//       if(critical == 4){
+//         for(var x = 0; x < 2; x++){ // yes critical
+//           userHP = userHP - 30;
+//         }
+//       }
+//       else{
+//         userHP = userHP - 30; // no critical
+//       }
+//   if(userHP < 0) { userHP = 0} // faint
+//   document.getElementById('myHP').innerHTML = userHP; // update hp
+//     if(userHP == 0) { // fainted
+//       document.getElementById('message').innerHTML = " volume at zero! " // fainted
+//     }
+//   }
+
+// updatehealth();
+// }
+
+// function dragonClaw() {
+//   var miss = Math.floor((Math.random() * 10) + 1);
+//   if(miss == 1 ) {
+//     document.getElementById('message').innerHTML = " anthony's attack missed! "
+//   }
+//   else {
+//   document.getElementById('message').innerHTML = " anthony used dragon claw "
+//   var critical = Math.floor((Math.random() * 10) + 1);
+//     if(critical == 4){
+//       for(var x = 0; x < 2; x++){
+//         userHP = userHP - 20;
+//       }
+//     }
+//     else{
+//       userHP = userHP - 20;
+//     }
+//   if(userHP < 0) { userHP = 0}
+//   document.getElementById('myHP').innerHTML = userHP;
+//     if(userHP == 0){
+//       document.getElementById('message').innerHTML = " sylveon fainted! "
+//     }
+//   }
+
+// updatehealth();
+// }
+
+// function ember() {
+//   var miss = Math.floor((Math.random() * 10) + 1);
+//   if(miss == 1 ) {
+//     document.getElementById('message').innerHTML = " anthony's attack missed! "
+//   }
+//   else {
+//   document.getElementById('message').innerHTML = " anthony used ember "
+//   var critical = Math.floor((Math.random() * 10) + 1);
+//     if(critical == 4){
+//       for(var x = 0; x < 2; x++){
+//         userHP = userHP - 10;
+//       }
+//     }
+//     else{
+//       userHP = userHP - 10;
+//     }
+//   if(userHP < 0) { userHP = 0}
+//   document.getElementById('myHP').innerHTML = userHP;
+//     if(userHP == 0){
+//       document.getElementById('message').innerHTML = " sylveon fainted! "
+//     }
+//   }
+
+// updatehealth();
+// }
+
+// function growl() {
+//   var miss = Math.floor((Math.random() * 10) + 1);
+//   if(miss == 1 ) {
+//     document.getElementById('message').innerHTML = " anthony's attack missed! "
+//   }
+//   else {
+//   document.getElementById('message').innerHTML = " anthony used growl "
+//   var critical = Math.floor((Math.random() * 10) + 1);
+//     if(critical == 4){
+//       for(var x = 0; x < 2; x++){
+//         userHP = userHP - 5;
+//       }
+//     }
+//     else{
+//       userHP = userHP - 5;
+//     }
+//   if(userHP < 0) { userHP = 0}
+//   document.getElementById('myHP').innerHTML = userHP;
+//     if(userHP == 0){
+//       document.getElementById('message').innerHTML = " sylveon fainted! "
+//     }
+//   }
+
+// updatehealth();
+// }
+
+
+
+// function compPokemon() { // continue
+//   if(playerMove == 1 && opHP != 0) { // whos move
+//   var move = Math.floor((Math.random() * 4) + 1); // choose move randomly
+//     opAttacks[move](); // call attack from array
+//     playerMove = 0; // update player move
+//     updateHealthBar();
+//   }
+// }
 
 ////////// ACTUAL VOLUME CONTROLS /////////////////
 
-// Get the health bar progress element
-const healthBarProgress = document.getElementById('health-bar-progress');
 
-// Set the initial health value to 100
-let health = 100;
+// // Set the initial health value to 100
+// let health = 90;
 
-// Function to update the health bar
-function updateHealthBar() {
-  // Set the width of the progress bar based on the health value
-  healthBarProgress.style.width = health + '%';
+// // Function to update the health bar
+// function updateHealthBar() {
+//   // Set the width of the progress bar based on the health value
+//  
   
-  // If the health is less than or equal to 0, end the game
-  if (health <= 0) {
-    alert('Game over!');
-    // Add more logic to end the game
-  }
-}
+//   // If the health is less than or equal to 0, end the game
+//   if (userHP = 10) {
+  
+//   }
+// }
 
-// Call the updateHealthBar function to initialize the health bar
-updateHealthBar();
+// // Call the updateHealthBar function to initialize the health bar
+// updateHealthBar();
 
-// Example code to decrease the health by 10% when the player is hit
-health -= 10;
-updateHealthBar();
+// // Example code to decrease the health by 10% when the player is hit
+// health -= 10;
+// updateHealthBar();

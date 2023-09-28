d11ab044195bb8f9ba514abae017355c51f8ce44
ana <anabellemahadevan@gmail.com>
2023-09-28 13:48:43 -0600

    mepe

--- atlas4630/quiz.js
+++ atlas4630/quiz.js
@@ -1,36 +1,40 @@
 function quiz() {
-    const questions = [
-      'How many moons does Earth have?',
-      'How many moons does Saturn have?',
-      'How many moons does Venus have?'
-    ];
 
-    console.log('1')
+  const score = 0
 
-    const answers = [1, 82, 0];
-    
-    const score = 0;
-  
-    let num = Math.floor(Math.random() * 1);
-  
-    for (let i = 0; i <= questions.length; i++) {
-      const question = questions[num];
-      const answer = prompt(question);
-      if (answer == answers[num]) {
-        score+=
-        alert('Correct!');
-      } else {
-        alert('Wrong!');
-      }
-      num++;
-      if (num === questions.length) {
-        num = 0;
-      }
+  const questions = [
+    'How many moons does Earth have?',
+    'How many moons does Saturn have?',
+    'How many moons does Venus have?',
+  ]
+
+  const answers = [1, 82, 0]
+
+  let num = Math.floor(Math.random() * 3)
+
+  for (let i = 0; i <= (questions.length - 1); i++) {
+    console.log('index', i, 'num', num)
+    const question = questions[num]
+    const answer = prompt(question)
+    console.log(answer, answers[num])
+    if (answer === answers[num]) {
+      score+=
+      alert('Correct!')
+    } 
+    else {
+      alert('Wrong!')
     }
-  
-    const scoreArea = document.querySelector('score-area');
-    scoreArea.innerHTML = `You got ${score} out of ${questions.length} questions correct!`;
+    if (num === questions.length) {
+      num = 0
+    }
+    else {
+      num++;
+    }
   }
-  
-  alert('Are you ready for a quiz?');
-  quiz();
+
+  const scoreArea = document.getElementById("score-area")
+  scoreArea.innerHTML = `You got ${score} out of ${questions.length} questions correct!`
+}
+
+// alert('Are you ready for a quiz?')
+quiz()

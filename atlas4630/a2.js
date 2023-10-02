1b3b524ea35f302cd8980922daf55579e91d06b3
ana <anabellemahadevan@gmail.com>
2023-10-02 11:16:55 -0600

    push 2 correction

--- atlas4630/a2.js
+++ atlas4630/a2.js
@@ -1,5 +1,8 @@
 function print() {
-    document.getElementById('display').innerHTML = 
-                document.getElementById("input").value;
-                document.getElementById("name").value;
+    document.getElementById('display').innerHTML = `
+    ${document.getElementById("input").value}
+    ${document.getElementById("name").value}
+    ${document.getElementById("age").value}
+    ${document.getElementById("gender").value}
+    `;
 }

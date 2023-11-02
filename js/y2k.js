dd758f4b208d009e5c5341764761c59b1cc62c04
ana <anabellemahadevan@gmail.com>
2023-11-02 10:27:48 -0600

    updates

--- js/y2k.js
+++ js/y2k.js
@@ -1,90 +1,90 @@
 
-//bubble cursor
-    var b = document.body;
-    var imgurl = "https://i.imgur.com/hgX29BX.gif";
-    var size = [10, 20];
-    function rand(min, max)
-    {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    function getSize()
-    {
-    return rand(size[0], size[1]);
-    }
-    function lerp(a,b,f)
-    {
-    return (b-a)*f+a;
-    }
-    function heart(x,y)
-    {
-    var s = getSize();
-    x -= s/2;
-    y -= s/2;
-    x = Math.floor(x) + rand(-5, 5);
-    y = Math.floor(y) + rand(-5, 5);
-    var fx = x + rand(-40, 40);
-    var fy = y + rand(-40, 40);
-    var i = document.createElement("img");
-    i.src = imgurl;
-    i.style = `pointer-events: none; position: fixed; width: ${s}px; left: ${x}px; top: ${y}px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 1000000;`;
-    b.appendChild(i);
-    var f = 0;
-    var interval;
-    interval = setInterval(function() {
-    var _x = Math.floor(lerp(x, fx, f));
-    var _y = Math.floor(lerp(y, fy, f));
-    i.style = `pointer-events: none; position: fixed; width: ${s}px; left: ${_x}px; top: ${_y}px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 1000000;`;
-    i.style.opacity = 1-f;
-    f += 0.01;
-    if (f > 1)
-    {
-    clearInterval(interval);
-    b.removeChild(i);
-    }
-    }, 10);
-    }
-    function bro(x,y)
-    {
-    for (var i = 0; i < 5; i++)
-    {
-    heart(x,y);
-    }
-    }
-    b.addEventListener("click", function(event){
-    var x = event.clientX;
-    var y = event.clientY;
-    bro(x,y);
-    });
+// bubble cursor
 
+const body = document.body;
+const cursorsrc = "/img/y2k/bubblecursor.gif";
+const sizerange = [10, 20];
 
-//     <script type="text/javascript">
-// marqueeInit({
-// 	uniqueid: 'mycrawler',
-// 	style: {
-// 		'padding': '0px',
-// 		'width': '1000px',
-// 		'background': '#ffeff9',
-// 		'border':'2px inset #ffddec',
-// 		'color':'#ff89bc',
-// 		'text-shadow': '-0.5px 0 #ff70ae, 0 0.5px #ff70ae, 0.5px 0 #ff70ae, 0 -0.5px #ff70ae'
-// 	},
-// 	inc: 4, //speed - pixel increment for each iteration of this marquee's movement
-// 	mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
-// 	moveatleast: 2,
-// 	neutral: 150,
-// 	persist: false,
-// 	savedirection: true
-// });
-// </script>
-
-
-{/* <script>
-var myVar = setInterval(function() {
-  myTimer();
-}, 1000);
-
-function myTimer() {
-  var d = new Date();
-  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-</script> */}
+
+function getSize() {
+    return rand(sizerange[0], sizerange[1]);
+}
+
+function createHeart(x, y, size) {
+    const heartElement = document.createElement("img");
+    heartElement.src = cursorsrc;
+    heartElement.style.cssText = `pointer-events: none; position: fixed; width: ${size}px; left: ${x}px; top: ${y}px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 1000000;`;
+    body.appendChild(heartElement);
+    return heartElement;
+}
+
+function animateHeart(heartElement, startX, startY, endX, endY) {
+    console.log("here1")
+    gsap.to(heartElement, {
+        x: endX,
+        y: endY,
+        opacity: 0,
+        duration: 5,
+        onComplete: () => {
+            body.removeChild(heartElement);
+        }
+    });
+}
+
+function createHearts(x, y, count) {
+    for (let i = 0; i < count; i++) {
+        const size = getSize();
+        const heartElement = createHeart(x - size / 2, y - size / 2, size);
+        const endX = x + rand(-40, 40);
+        const endY = y + rand(-40, 40);
+        animateHeart(heartElement, x, y, endX, endY);
+    }
+}
+
+body.addEventListener("click", function(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    createHearts(x, y, 5);
+});
+
+///////// clock! /////////////
+
+function updateClock() {
+    const d = new Date();
+    document.getElementById("clock").textContent = d.toLocaleTimeString();
+}
+
+// constantly updates clock
+const intervalId = setInterval(updateClock, 1000);
+
+//////// scroll animation  ////////////
+
+const marqueeElement = document.getElementById('crawl-inner');
+const marqueeWidth = 1000;
+const marqueeSpeed = 0.2; // speed of marquee scrolling
+
+gsap.to(marqueeElement, {
+    x: -marqueeWidth,
+    ease: "none",
+    repeat: -1,
+    modifiers: {
+        x: function(x) {
+            return (x < -marqueeWidth) ? marqueeWidth : x;
+        }
+    },
+    scrollTrigger: {
+        trigger: marqueeElement,
+        start: "left bottom",
+        scrub: true
+    }
+});
+
+
+
+
+// making elements draggable here 
+
+Draggable.create("#digiwindow");
